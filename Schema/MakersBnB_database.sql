84e1fd0447cd630f37342db887d35fec4dfa880e
Khuslen Ganzorig <khusy.gan@gmail.com>
2023-06-07 11:49:05 +0100

    Added bookings and availability tables to schema

--- Schema/MakersBnB_database.sql
+++ Schema/MakersBnB_database.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS users, spaces;
+DROP TABLE IF EXISTS users, spaces, bookings, availability;
 
 
 -- Table Definition
@@ -53,4 +53,25 @@ INSERT INTO users (username, email, password) VALUES
 INSERT INTO spaces (name, city, description, price, start_date, end_date, created_at, updated_at, user_id) VALUES
 ('London Plaza', 'London', '2 bedroom flat', '60.50', '2023-06-05', '2023-06-20', NOW(), NOW(), 1),
 ('Paris Cottage', 'Paris', 'Entire house', '30.50', '2023-07-05', '2023-07-20', NOW(), NOW(), 2);
+
+INSERT INTO bookings (space_id, user_id, start_date, end_date, contents, status) VALUES
+(1, 1, '2023-06-05', '2023-06-10', 'I would like an extra bed', 'pending'),
+(2, 2, '2023-07-05', '2023-07-11', 'No requests', 'approved'); 
+
+
+INSERT INTO availability (space_id, date, is_available) VALUES
+(1, '2023-06-05', true),
+(1, '2023-06-06', true),
+(1, '2023-06-07', true),
+(1, '2023-06-08', true),
+(1, '2023-06-09', true),
+(2, '2023-07-05', false),
+(2, '2023-07-06', false),
+(2, '2023-07-07', false),
+(2, '2023-07-08', false),
+(2, '2023-07-09', false),
+(2, '2023-07-10', false);
+
+
+
 
---
+++ lib/models/booking.rb
@@ -0,0 +1,3 @@
+class Booking
+    attr_accessor :id, :space_id :user_id, :start_date, :end_date, :contents, :status
+  end     
